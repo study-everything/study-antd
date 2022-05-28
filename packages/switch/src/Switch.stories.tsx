f21a0bd54c55854f83b690096e356cc21a1d8f24
yexiaolong <771388996@qq.com>
2022-05-28 18:08:00 +0800

    1.添加switch组件packages包相关

---
+++ packages/switch/src/Switch.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Switch }from './Switch';
+import './style'
+
+storiesOf('Switch', module).add('Demo', () => (	
+	<>
+		<Switch />
+	</> 
+))
