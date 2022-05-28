3faa74eac0fd5e130a6a624a269e7603578f1e50
vitalboyzf <zzzfff2021>
2022-05-28 21:05:34 +0800

    input组件结构搭建

---
+++ packages/input/src/Input.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Input }from './Input';
+import './style'
+
+storiesOf('Input', module).add('Demo', () => (	
+	<>
+		<Input />
+	</> 
+))
