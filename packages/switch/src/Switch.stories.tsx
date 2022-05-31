ac4b1c9372eb5e316f66f8d2fad4944552200392
yexiaolong <771388996@qq.com>
2022-05-31 22:39:20 +0800

    1.完成Switch Size和Disabled;

--- packages/switch/src/Switch.stories.tsx
+++ packages/switch/src/Switch.stories.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React, {useState} from 'react';
+import type {ComponentMeta, ComponentStory} from '@storybook/react';
+import { Button } from 'antd';
+import './Switch.stories.less';
 import { Switch }from './Switch';
 import './style'
 
-storiesOf('Switch', module).add('Demo', () => (	
-	<>
-		<Switch />
-	</> 
-))
+export default {
+	title: 'Switch',
+	component: Switch,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	}
+} as ComponentMeta<typeof Switch>
+
+const switchWithSize = () => (
+	<div>
+		<div>
+			<Switch />
+		</div>
+		<div>
+			<Switch size="small" />
+		</div>
+	</div>
+)
+
+export const SwitchWithSize: ComponentStory<typeof Switch> = () => switchWithSize();
+SwitchWithSize.storyName = 'Size'
+
+const SwitchWithDisable = () => {
+	const [disableFlag, setDisableFlag] = useState<boolean>(true);
+	const toggleDisable = () => {
+		setDisableFlag(!disableFlag);
+	}
+	return (
+		<div>
+			<Switch defaultChecked disabled={disableFlag}/>
+			<div className='toggle-btn'>
+				<Button type="primary" onClick={toggleDisable}>Toggle disabled</Button>
+			</div>
+		</div>
+	)
+}
+export const switchWithDisable: ComponentStory<typeof Switch> = () => SwitchWithDisable();
+switchWithDisable.storyName = 'Disabled'
