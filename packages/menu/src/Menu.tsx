918ea0095d4d3a6f431b3339820b5db17adabe54
yexiaolong <771388996@qq.com>
2022-06-01 22:40:53 +0800

    1.添加menu目录文件夹;2.完成switch Event事件、InnerContent、Loading;

---
+++ packages/menu/src/Menu.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export interface MenuProps {
+
+}
+
+
+export const Menu: React.FC<MenuProps> = (props) =>
+	// TODO
+	 <div>Menu</div>
--- packages/switch/src/Switch.stories.tsx
+++ packages/switch/src/Switch.stories.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import type {ComponentMeta, ComponentStory} from '@storybook/react';
 import { Button } from 'antd';
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
 import './Switch.stories.less';
 import { Switch }from './Switch';
 import './style'
@@ -43,3 +44,50 @@ const SwitchWithDisable = () => {
 }
 export const switchWithDisable: ComponentStory<typeof Switch> = () => SwitchWithDisable();
 switchWithDisable.storyName = 'Disabled'
+
+const SwitchWithFuc = () => {
+	const switchClickHandle = (switchFlag) => {
+		setTimeout(() => alert(`您点击了Switch，当前的状态是${switchFlag}`), 300);
+	}
+	const switchChangeHandle = (switchFlag) => {
+		setTimeout(() => alert(`Switch change触发，当前的状态是${switchFlag}`), 300);
+	}
+	return (
+		<div>
+			<div>可以进行小键盘左右切换控制</div>
+			<Switch
+				defaultChecked
+				onClick={switchClickHandle}
+				onChange={switchChangeHandle}
+			/>
+		</div>
+	)
+}
+export const switchWithFuc: ComponentStory<typeof Switch> = () => SwitchWithFuc();
+switchWithFuc.storyName = 'Event';
+
+const SwitchWithInnerContent = () => (
+		<>
+			<Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked />
+			<div className='middle-section'>
+				<Switch checkedChildren="1" unCheckedChildren="0" />
+			</div>
+			<Switch
+				checkedChildren={<CheckOutlined />}
+				unCheckedChildren={<CloseOutlined />}
+				defaultChecked
+			/>
+		</>
+	)
+export const switchWithInnerContent: ComponentStory<typeof Switch> = () => SwitchWithInnerContent();
+switchWithInnerContent.storyName = 'InnerContent';
+
+const SwitchWithLoading = () => (
+		<>
+			<Switch loading defaultChecked />
+			<br />
+			<Switch size="small" loading />
+		</>
+	)
+export const switchWithLoading: ComponentStory<typeof Switch> = () => SwitchWithLoading();
+switchWithLoading.storyName = 'Loading';
