6acc9aed005110fb583c36fa319de835fac64d41
s z <zhangshuo>
2022-06-04 13:24:06 +0800

    feat: 完成empty组件

---
+++ packages/empty/src/utils.ts
@@ -0,0 +1,5 @@
+export const getPrefixCls = (suffixCls?: string, customizePrefixCls?: string) => {
+    if (customizePrefixCls) return customizePrefixCls;
+    return suffixCls ? `ant-${suffixCls}` : 'ant';
+};
+export const direction = "rtl";
