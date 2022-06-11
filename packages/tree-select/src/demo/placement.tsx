6d54d029eb1358b66539ca45eefad65d1a2fcd69
brolly <102495553@qq.com>
2022-06-11 23:01:07 +0800

    feat: tree-select组件开发

--- packages/tree-select/src/demo/placement.tsx
+++ packages/tree-select/src/demo/placement.tsx
@@ -1,16 +1,13 @@
-import type { RadioChangeEvent } from 'antd';
 import { Radio } from 'antd';
-import TreeSelect from '../TreeSelect'
-import type { SelectCommonPlacement } from 'antd/es/_util/motion';
 import React, { useState } from 'react';
+import TreeSelect from '../TreeSelect'
 
 const { TreeNode } = TreeSelect;
 
-const App: React.FC = () => {
-  const [placement, SetPlacement] = useState<SelectCommonPlacement>('topLeft');
+const App = () => {
+  const [placement, SetPlacement] = useState('topLeft');
 
-  const placementChange = (e: RadioChangeEvent) => {
-    console.log('placement', e.target.value)
+  const placementChange = (e) => {
     SetPlacement(e.target.value);
   };
 
@@ -27,12 +24,16 @@ const App: React.FC = () => {
 
       <TreeSelect
         showSearch
-        dropdownStyle={{ maxHeight: 400, overflow: 'auto', minWidth: 300 }}
+        dropdownStyle={{
+          maxHeight: 400,
+          overflow: 'auto',
+          minWidth: 300,
+        }}
         placeholder="Please select"
         dropdownMatchSelectWidth={false}
         placement={placement}
         allowClear
-        treeDefaultExpandAll
+        
       >
         <TreeNode value="parent 1" title="parent 1">
           <TreeNode value="parent 1-0" title="parent 1-0">
@@ -40,7 +41,18 @@ const App: React.FC = () => {
             <TreeNode value="leaf2" title="leaf2" />
           </TreeNode>
           <TreeNode value="parent 1-1" title="parent 1-1">
-            <TreeNode value="leaf3" title={<b style={{ color: '#08c' }}>leaf3</b>} />
+            <TreeNode
+              value="leaf3"
+              title={
+                <b
+                  style={{
+                    color: '#08c',
+                  }}
+                >
+                  leaf3
+                </b>
+              }
+            />
           </TreeNode>
         </TreeNode>
       </TreeSelect>
