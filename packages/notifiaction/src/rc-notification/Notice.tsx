b2595dde4dfc73bed2ecb0137baa308c5e3873ac
lihaohao <2495041749@qq.com>
2022-06-20 21:30:00 +0800

    feat: rc-notification 实现

---
+++ packages/notifiaction/src/rc-notification/Notice.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import classNames from 'classnames';
+
+export interface NoticeConfig {
+  content?: React.ReactNode;
+  duration?: number | null;
+  closeIcon?: React.ReactNode;
+  closable?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  /** @private Internal usage. Do not override in your code */
+  props?: React.HTMLAttributes<HTMLDivElement> & Record<string, any>;
+
+  onClose?: VoidFunction;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+export interface NoticeProps extends Omit<NoticeConfig, 'onClose'> {
+  prefixCls: string;
+  className?: string;
+  style?: React.CSSProperties;
+  eventKey: React.Key;
+
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  onNoticeClose?: (key: React.Key) => void;
+}
+
+const Notify = React.forwardRef<HTMLDivElement, NoticeProps>((props, ref) => {
+  const {
+    prefixCls,
+    style,
+    className,
+    duration = 4.5,
+
+    eventKey,
+    content,
+    closable,
+    closeIcon = 'x',
+    props: divProps,
+
+    onClick,
+    onNoticeClose,
+  } = props;
+  const [hovering, setHovering] = React.useState(false);
+
+  // ======================== Close =========================
+  const onInternalClose = () => {
+    onNoticeClose(eventKey);
+  };
+
+  // ======================== Effect ========================
+  // 表示关闭定时器
+  React.useEffect(() => {
+    if (!hovering && duration > 0) {
+      const timeout = setTimeout(() => {
+        onInternalClose();
+      }, duration * 1000);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [duration, hovering]);
+
+  // ======================== Render ========================
+  const noticePrefixCls = `${prefixCls}-notice`;
+
+  return (
+    <div
+      {...divProps}
+      ref={ref}
+      className={classNames(noticePrefixCls, className, {
+        [`${noticePrefixCls}-closable`]: closable,
+      })}
+      style={style}
+      onMouseEnter={() => {
+        setHovering(true);
+      }}
+      onMouseLeave={() => {
+        setHovering(false);
+      }}
+      onClick={onClick}
+    >
+      {/* Content */}
+      <div className={`${noticePrefixCls}-content`}>{content}</div>
+
+      {/* Close Icon */}
+      {closable && (
+        <a
+          tabIndex={0}
+          className={`${noticePrefixCls}-close`}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            onInternalClose();
+          }}
+        >
+          {closeIcon}
+        </a>
+      )}
+    </div>
+  );
+});
+
+export default Notify;
