bcb365b6e7f0b16e9e4a382d41e4766e8ee00bcf
guchen.yh <guchen.yh@alibaba-inc.com>
2022-07-05 13:26:11 +0800

    feat: 评论组件

---
+++ packages/comment/src/Comment.stories.tsx
@@ -0,0 +1,65 @@
+import React, { createElement, useState } from 'react';
+import './style'
+import { storiesOf } from '@storybook/react';
+import { Tooltip, Avatar } from 'antd';
+import moment from 'moment';
+import { DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled } from '@ant-design/icons';
+import { Comment }from './Comment';
+import './Comment.stories.less';
+
+storiesOf('Comment', module).add('Demo', () => {	
+	
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+    const [action, setAction] = useState<string | null>(null);
+  
+    const like = () => {
+      setLikes(1);
+      setDislikes(0);
+      setAction('liked');
+    };
+  
+    const dislike = () => {
+      setLikes(0);
+      setDislikes(1);
+      setAction('disliked');
+    };
+  
+    const actions = [
+      <Tooltip key="comment-basic-like" title="Like">
+        <span onClick={like}>
+          {createElement(action === 'liked' ? LikeFilled : LikeOutlined)}
+          <span className="comment-action">{likes}</span>
+        </span>
+      </Tooltip>,
+      <Tooltip key="comment-basic-dislike" title="Dislike">
+        <span onClick={dislike}>
+          {React.createElement(action === 'disliked' ? DislikeFilled : DislikeOutlined)}
+          <span className="comment-action">{dislikes}</span>
+        </span>
+      </Tooltip>,
+      <span key="comment-basic-reply-to">Reply to</span>,
+    ];
+  
+    return (
+      <Comment
+        actions={actions}
+        author={<a>Han Solo</a>}
+        avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" size={40} style={{width: "40px"}} />}
+        content={
+          <p>
+            We supply a series of design principles, practical patterns and high quality design
+            resources (Sketch and Axure), to help people create their product prototypes beautifully
+            and efficiently.
+          </p>
+        }
+        datetime={
+          <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+            <span>{moment().fromNow()}</span>
+          </Tooltip>
+        }
+      />
+    );
+})
+
+  
