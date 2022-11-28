daec2dee96f43c034fff2e865824d1cc3083474d
zdx76124 <761247812@qq.com>
2022-11-28 19:31:41 +0800

    feat: packages add collapse

---
+++ packages/collapse/src/CollapsePanel.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import classNames from 'classnames';
+import RcCollapse from 'rc-collapse';
+import { ConfigContext } from './config-provider';
+import warning from './_util/warning';
+
+export type CollapsibleType = 'header' | 'disabled';
+
+export interface CollapsePanelProps {
+  key: string | number;
+  header: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  showArrow?: boolean;
+  prefixCls?: string;
+  forceRender?: boolean;
+  id?: string;
+  extra?: React.ReactNode;
+  collapsible?: CollapsibleType;
+  children?: React.ReactNode;
+}
+
+const CollapsePanel: React.FC<CollapsePanelProps> = props => {
+  warning(
+    !('disabled' in props),
+    'Collapse.Panel',
+    '`disabled` is deprecated. Please use `collapsible="disabled"` instead.',
+  );
+
+  const { getPrefixCls } = React.useContext(ConfigContext);
+  const { prefixCls: customizePrefixCls, className = '', showArrow = true } = props;
+  const prefixCls = getPrefixCls('collapse', customizePrefixCls);
+  const collapsePanelClassName = classNames(
+    {
+      [`${prefixCls}-no-arrow`]: !showArrow,
+    },
+    className,
+  );
+  return <RcCollapse.Panel {...props} prefixCls={prefixCls} className={collapsePanelClassName} />;
+};
+
+export default CollapsePanel;
