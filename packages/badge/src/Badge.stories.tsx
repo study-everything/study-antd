832b4aa7a64b65fdbdf187edad38d67f77a7b590
alli <alli@trip.com>
2022-11-29 19:34:41 +0800

    feat: badge init

---
+++ packages/badge/src/Badge.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Badge }from './Badge';
+import './style'
+
+storiesOf('Badge', module).add('Demo', () => (	
+	<>
+		<Badge />
+	</> 
+))
---
+++ packages/badge/src/Badge.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export interface BadgeProps {
+
+}
+
+
+export const Badge: React.FC<BadgeProps> = (props) => {
+	// TODO
+	return <div>Badge</div>
+}
